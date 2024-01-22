9b08fb8ef0c018ee1c4ee769934d78aa6d9cb275
pasindu <madushan01722@gmail.com>
2024-01-22 16:22:40 +0530

    create Login_Form.fxml file

---
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -0,0 +1,17 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginFormController {
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+}
