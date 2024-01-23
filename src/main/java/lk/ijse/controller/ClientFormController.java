b1dc43e0a1051e7331889bb1660fd8fa67491920
pasindu <madushan01722@gmail.com>
2024-01-23 11:30:44 +0530

    create Client_Form.fxml file

---
+++ src/main/java/lk/ijse/controller/ClientFormController.java
@@ -0,0 +1,39 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class ClientFormController {
+
+    @FXML
+    private Label lblUserName;
+
+    @FXML
+    private ScrollPane scrollPain;
+
+    @FXML
+    private JFXTextField txtMassage;
+
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    void btnCameraOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmojiOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSendOnAction(ActionEvent event) {
+
+    }
+
+}
